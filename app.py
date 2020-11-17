1a71008f102060fc5975c7b24a43260b4b26fef4
Mike Connor <mike@sparkgeo.com>
2020-11-17 15:55:04 -0800

    Add ingest roles and minor changes.

--- app.py
+++ app.py
@@ -10,13 +10,18 @@ DEPLOY_ENV = core.Environment(account='138863487738', region='us-east-1')
 
 app = core.App()
 
+##  Development Stacks  ##
+
 # Infrastructure
 infra = BaseInfrastructure(
-    app, 'base-infrastructure', cidr_block='10.0.0.0/16', env=DEPLOY_ENV
+    app, 'dev-base-infrastructure', cidr_block='10.0.0.0/16', env=DEPLOY_ENV
 )
 
 # Applications
-SampleLambdaIngestStack(app, "sample-lambda-ingest-dev", env=DEPLOY_ENV)
-SampleJobIngestStack(app, 'sample-job-ingest-dev', env=DEPLOY_ENV)
+SampleLambdaIngestStack(app, "dev-sample-lambda-ingest-dev", env=DEPLOY_ENV)
+SampleJobIngestStack(app, 'dev-sample-job-ingest-dev', env=DEPLOY_ENV)
+
+## Production Stacks ##
+
 
 app.synth()
