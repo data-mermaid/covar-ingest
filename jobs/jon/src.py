c125b8c2382eb4146304bdbd68975f43a0920f8f
jonhealy1 <jonathan.d.healy@gmail.com>
2020-11-17 17:08:20 -0800

    convert to cogs works with docker

--- jobs/jon/src.py
+++ jobs/jon/src.py
@@ -1,5 +1,6 @@
 import boto3
 import urllib.request
+from osgeo import gdal
 
 # https://pae-paha.pacioos.hawaii.edu/erddap/griddap/dhw_5km.geotif?CRW_DHW%5B(2020-11-15T12:00:00Z):1:(2020-11-15T12:00:00Z)%5D%5B(89.975):1:(-89.975)%5D%5B(-179.975):1:(179.975)%5D
 # dhw_5km_00c3_7b6f_8b8a.tif
@@ -15,6 +16,8 @@ def invoke():
         datatowrite = filedata.read()
         with open(filepath, 'wb') as f:
             f.write(datatowrite)
+        print("starting cog transformation")
+        cog()
     except urllib.error.URLError as e:
         print('The server couldn\'t fulfill the request.')
         print('Error code: ', e.code)
@@ -23,6 +26,48 @@ def invoke():
         print('Reason: ', e.reason)
 
     print("hello")
+
+def cog():
+    input_tif_dir = "data/"
+    input_tif = f"{input_tif_dir}/dhw_5km_00c3_7b6f_8b8a.tif"
+    output_cog = f"{input_tif_dir}/dhw_5km_00c3_7b6f_8b8a_cog.tif"
+    tif_to_cog(input_tif, output_cog)
+
+def tif_to_cog(input_tif, output_cog):
+    data = gdal.Open(input_tif)
+    data_geotrans = data.GetGeoTransform()
+    data_proj = data.GetProjection()
+    data_array = data.ReadAsArray()
+    x_size = data.RasterXSize
+    y_size = data.RasterYSize
+    num_bands = data.RasterCount
+    datatype = data.GetRasterBand(1).DataType
+    data = None
+    driver = gdal.GetDriverByName('MEM')
+    data_set = driver.Create('', x_size, y_size, num_bands, datatype)
+    for i in range(num_bands):
+        data_set_lyr = data_set.GetRasterBand(i + 1)
+        if len(data_array.shape) == 2:
+            data_set_lyr.WriteArray(data_array)
+        else:
+            data_set_lyr.WriteArray(data_array[i])
+    data_set.SetGeoTransform(data_geotrans)
+    data_set.SetProjection(data_proj)
+    data_set.BuildOverviews("NEAREST", [2, 4, 8, 16, 32, 64])
+    driver = gdal.GetDriverByName('GTiff')
+    data_set2 = driver.CreateCopy(
+        output_cog,
+        data_set,
+        options = [
+                "COPY_SRC_OVERVIEWS=YES",
+                "TILED=YES",
+                "COMPRESS=LZW"
+            ]
+        )
+    data_set = None
+    data_set2 = None
+
+
 
 if __name__ == "__main__":
     invoke()
