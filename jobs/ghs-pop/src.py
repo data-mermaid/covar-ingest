e53846f9a8c650b3d52489a7108ccbc7920c3af6
Dana <Dana>
2020-11-19 17:18:29 -0800

    Download function

--- jobs/ghs-pop/src.py
+++ jobs/ghs-pop/src.py
@@ -1,4 +1,21 @@
 import boto3
+import requests
+import zipfile
+from io import BytesIO
+
+def download(url):
+    r = requests.get(url, )
+
+    if r.status_code == 200:
+        if r.headers['content-type'] == 'application/zip':
+            z = zipfile.ZipFile(BytesIO(r.content))
+            return z
+        else:
+            print ("Wrong file type")
+            return
+    else:
+        print('File not downloaded. Encountered error code', r.status_code)
+        return
 
 
 def invoke():
